925192ae93c0b60aaae494e8333f9cd7c34a3b5d
Sho Hashimoto <hashimoto@shokai.org>
2011-03-07 19:43:05 +0900

    urlmemo.rb

---
+++ lib/urlmemo.rb
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'net/http'
+require 'uri'
+
+class UrlMemo
+  
+  def initialize(api_url)
+    @api_uri = URI.parse api_url
+  end
+
+  def add(params)
+    query = params.map{|k,v|
+      "#{k}=#{v}"
+    }.join('&')
+    res = nil
+    Net::HTTP.start(@api_uri.host, @api_uri.port){|http|
+      res = http.post(@api_uri.path, query)
+    }
+    return res
+  end
+end
+
+if __FILE__ == $0
+  u = UrlMemo.new 'http://localhost:8087/'
+  res = u.add(:url => 'http://shokai.org/blog/',
+              :title => 'shokai blog')
+  p res
+  require 'json'
+  p JSON.parse(res.body)
+end
